a0cd1239a4f05e50535e17aa2d913b3406de5de4
cintick <drummercintick@gmail.com>
2023-11-30 15:46:28 +0300

    30kasim

---
+++ Assets/Scripts/CarController2.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController2 : MonoBehaviour {
+
+    // Settings
+    public float MoveSpeed = 50;
+    public float MaxSpeed = 15;
+    public float Drag = 0.98f;
+    public float SteerAngle = 20;
+    public float Traction = 1;
+    public float MoveVectorCorrection;
+
+    // Variables
+    private Vector3 MoveForce;
+
+    // Update is called once per frame
+    void Update() {
+
+        // Moving
+        MoveForce += transform.forward * MoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position += MoveForce * Time.deltaTime;
+
+        // Steering
+        float steerInput = Input.GetAxis("Horizontal");
+        transform.Rotate(transform.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+        // ESKI HALI transform.Rotate(Vector3.up * steerInput * MoveForce.magnitude * SteerAngle * Time.deltaTime);
+
+        // Drag and max speed limit
+        MoveForce *= Drag;
+        MoveForce = Vector3.ClampMagnitude(MoveForce, MaxSpeed);
+
+        // Traction
+        Debug.DrawRay(transform.position, MoveForce.normalized * 15);
+        Debug.DrawRay(transform.position, transform.forward * 15, Color.blue);
+        MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveVectorCorrection;
+
+        //MoveForce ESKI HALI
+        //MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, Traction * Time.deltaTime) * MoveForce.magnitude;
+
+    }
+}
